896f3e17ff72b57cfb3fa1385cc7f9fd238d797d
jakaria kabir <jkp.jakaria@gmail.com>
2023-05-22 20:41:54 +0600

    initial commit

---
+++ new-app/app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AssignmentController extends Controller
+{
+    // Question 1 answer
+    function UserInfo(Request $request):string{
+        $name = $request->input('name');
+        return $name;
+    }
+
+    // Question 2 answer
+    function UserAgent(Request $request){
+        $userAgent = $request->header('UserAgent');
+        return $userAgent;
+    }
+
+    // Question 3 answer
+    function ApiEndpoint(Request $request){
+        $page = $request->query('page',"null");
+        if($page !== null){
+            return $page;
+        }else{
+            return "null";
+        }
+    }
+
+    // Question 4 answer
+    function JsonResponse():JsonResponse{
+        $data = array(
+            "message"=> "Success",
+            "data"=>array(
+                "name"=> "Jakaia kabir",
+                "age"=> 22
+            )
+        );
+        return response()->json($data);
+    }
+
+    // Question 5 answer
+    function FileUploads(Request $request){
+        $file=$request->file('avatar');
+        $file->move(public_path('uploads'),$file->getClientOriginalName());
+        return "file uploaded";
+    }
+
+    // Question 6 answer
+    function SetCookie(Request $request){
+        $rememberToken = $request->cookie('remember_token',null);
+        return $rememberToken;
+    }
+}
+
